677eb4b8c7e6acf636659d5beabb5cdca00e64af
wangare <marywangareee@gmail.com>
2022-07-28 10:00:23 +0300

    first commit

--- app/src/main/java/mary/dev/fibonnica/MainActivity.kt
+++ app/src/main/java/mary/dev/fibonnica/MainActivity.kt
@@ -4,33 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import mary.dev.fibonnica.databinding.ActivityMainBinding
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var numberList= Addition(100)
-        println(numberList)
-        binding.rvnumber.layoutManager=LinearLayoutManager(this)
-        binding.rvnumber.adapter=NumberRecyclerViewAdater(numberList)
+        displayNumber()
+    }
+
+    fun displayNumber() {
+        var fiblist = generate100fibonacci()
+        val numberlist = NumberRecyclerViewAdater(fiblist)
+        binding.rvnumber.layoutManager = LinearLayoutManager(this)
+        binding.rvnumber.adapter = numberlist
+
 
     }
-    fun Addition(size:Int):List<Int> {
-        var list = ArrayList<Int>()
-        var fn1 = 0
-        var fn2 = 1
-        var number = 0
-        while (number <= size) {
-            print(fn1)
-            var add = fn1 + fn2
-            fn1 = fn2
-            fn2 = add
-            number++
-            list += add
+
+    fun generate100fibonacci(): MutableList<BigInteger> {
+        var fiblist: MutableList<BigInteger> = mutableListOf(BigInteger.ZERO, BigInteger.ONE)
+        while (fiblist.size < 100) {
+            var last = fiblist.get(fiblist.lastIndex)
+            var secondlast = fiblist.get(fiblist.lastIndex - 1)
+            var next = last + secondlast
+            fiblist.add(next)
+
         }
-        return list
+        return fiblist
     }
 }
+
